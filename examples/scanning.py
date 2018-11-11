cc32262655fb5be4123fb820c1be057e82858cb4
Kent Williams <k3nt00@gmail.com>
2018-11-11 14:37:32 -0800

    First bluezdbus scanning implementation working

--- examples/scanning.py
+++ examples/scanning.py
@@ -1,18 +1,18 @@
 import asyncio
 from aioble import CentralManager
 
-def callback(args):
-    print(args)
+def callback(address, name):
+    print("Address: " + address + " Name: " + name)
 
 async def scan():
     cm = CentralManager()
     print('Scanning for BLE devices...')
     try:
         print('Starting')
-        await cm.start_scan(callback)
+        await cm.start_scan(callback, timeout_sec = 2)
     finally:
         print('Stopping')
-        await cm.stop_scan()
+        await cm.stop_scan(timeout_sec = 1)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(scan())
