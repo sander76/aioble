3503253be2b249a3ae9c924191b2803147a7012f
Jason Dreisbach <jtdreisb@gmail.com>
2019-03-24 20:23:29 -0700

    add characteristic capabilities (#21)

--- aioble/corebluetooth/characteristic.py
+++ aioble/corebluetooth/characteristic.py
@@ -43,4 +43,38 @@ class CoreBluetoothCharacteristic(Characteristic):
             else:
                 future.set_result(self._cbcharacteristic.value())
 
+    async def read_value(self):
+        if not self.can_read:
+            self._read_value_future = asyncio.Future()
+            await self._read_value()
+            value = await self._read_value_future
+            return value
+        else:
+            return None
 
+    def can_broadcast(self):
+        return self._cbcharacteristic.properties() & CoreBluetooth.CBCharacteristicPropertyBroadcast
+
+    def can_read(self):
+        return self._cbcharacteristic.properties() & CoreBluetooth.CBCharacteristicPropertyRead
+
+    def can_write(self):
+        return self._cbcharacteristic.properties() & CoreBluetooth.CBCharacteristicPropertyWrite
+
+    def can_notify(self):
+        return self._cbcharacteristic.properties() & CoreBluetooth.CBCharacteristicPropertyNotify
+
+    def can_write_noreply(self):
+        return self._cbcharacteristic.properties() & CoreBluetooth.CBCharacteristicPropertyWriteWithoutResponse
+
+    @util.dispatched_to_queue(wait=False)
+    def _read_value(self):
+        self._cbperipheral.readValueForCharacteristic_(self._cbcharacteristic)
+
+    def _did_update_value(self, error : Foundation.NSError):
+        future = self._read_value_future
+        if future is not None:
+            if error is not None:
+                future.set_exception(util.NSErrorException(error))
+            else:
+                future.set_result(self._cbcharacteristic.value())
