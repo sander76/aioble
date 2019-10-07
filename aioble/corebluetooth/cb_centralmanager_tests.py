26c9c315c5c3122039d86050bd26fed399e1dff6
Jason Dreisbach <jasondreisbach@me.com>
2019-10-06 22:10:21 -0700

    Mock out platform specific dependencies so unit tests can run on any platform (#30)
    * Mock out platform specific imports so unit tests can be run on any platform
    
    * add more mocks

--- aioble/corebluetooth/cb_centralmanager_tests.py
+++ aioble/corebluetooth/cb_centralmanager_tests.py
@@ -1,10 +1,16 @@
-from aioble.corebluetooth.cb_centralmanager import CoreBluetoothCentralManager
 import asyncio
 import pytest
+import unittest.mock
+import sys
+
+sys.modules['Foundation'] = unittest.mock.MagicMock()
+sys.modules['CoreBluetooth'] = unittest.mock.MagicMock()
+sys.modules['libdispatch'] = unittest.mock.MagicMock()
+sys.modules['objc'] = unittest.mock.MagicMock()
+
+from aioble.corebluetooth.cb_centralmanager import CoreBluetoothCentralManager
 
 pytestmark = pytest.mark.asyncio
-
-devices = []
 
 async def test_cb_centralmanager_initialization(event_loop: asyncio.AbstractEventLoop):
     manager = CoreBluetoothCentralManager()
